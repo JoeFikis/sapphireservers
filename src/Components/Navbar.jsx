ec81466aa7eaace1b875cc1439136ab6aaf81c4a
JoeFikis <aidanmg608@gmail.com>
2023-01-02 12:33:13 -0600

    work

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Styles/Navbar.css'
+import  {BrowserRouter} from 'react-router-dom'
 
 function Navbar() {
+
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+
+
   return (
     <div className='nav-container'>
         <div className='nav-wrapper'>
             <div className='nav-logo'>
                 <h1>SapphireServers</h1>
-                <i></i> 
+                <i class="fa-solid fa-server"></i>
             </div>
+            <div className="nav-menu">
             <div className="nav-links">
-                <a href="/" className='nav-btn'>Home</a>
-                <a href="/" className='nav-btn'>About</a>
-                <a href="/" className='nav-btn'>Contact</a>
+              <BrowserRouter>
+              <ul className='nav-links'>
+                <a href="/" className='nav-btn' onClick={handleClick}>Home</a>
+                <a href="../about.js" className='nav-btn' onClick={handleClick}>About</a>
+                <a href="/" className='nav-btn' onClick={handleClick}>Contact</a>
+                <a href="https://dash.sapphireservers.net" className='nav-btn'>Sign-Up</a>
+                <div className="dot"></div>
+              </ul>
+                </BrowserRouter>
             </div>
+            </div>
+            <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
         </div>
     </div>
   )
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './Components/Navbar.jsx'
+
+function app() {
+  return (
+    <Navbar />
+  )
+}
+
+export default app
