e19a9fd0eb7da783f29a145a905b88c55053b5ba
JoeFikis <aidanmg608@gmail.com>
2023-01-30 22:03:18 -0600

    work

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -1,48 +1,28 @@
 import React from 'react'
 import './footer.css'
+import { Column, Heading, Row } from './FooterStyles'
 
-function footer() {
+function Footer() {
   return (
-    <div className="footer-container">
-      <div className="wrapper">
-        <div className="links">
-        {/* Billing */}
-        <div className="billing">
-          <h2> Billing</h2>
-          <div className="plans">
-          <button><a href="/">Plans</a></button>
+    <div>
+      <div className="footer-container">
+        <div className="footer-wrapper">
+          <div className="footer-links">
+            {/* Billing Links */}
+            <div className="billing">
+              <Heading>Billing</Heading>
+              <a href="https://dash.sapphireservers.net/" className='Dashboard'>Dashboard</a>
+              </div>
+               {/* Legal Links */}
+            <div className="legal">
+              <Heading>Legal</Heading>
+              <a href="/" className='TOS'>TOS</a>
+              </div>
           </div>
-          <div className="billing_panel">
-            <button><a href="https://dash.sapphireservers.net">Billing Panel</a></button>
-          </div>
-        </div>
-        {/* Legal */}
-        <div className="legal">
-          <h2>Legal</h2>
-          <div className="TOS">
-          <button><a href="/">TOS</a></button>
-          </div>
-          <div className="privacy">
-            <button><a href="https://dash.sapphireservers.net">Privacy And Policy</a></button>
-          </div>
-        </div>
-        {/* Social */}
-        <div className="Social">
-          <h2>Soical</h2>
-          <div className="Discord">
-          <button><a href="/">Discord</a></button>
-          </div>
-          <div className="Yt">
-            <button><a href="https://dash.sapphireservers.net">Youtube</a></button>
-          </div>
-        </div>
         </div>
       </div>
-      <div className="name">
-          <h2>SapphireServers</h2>
-        </div>
     </div>
   )
 }
 
-export default footer
+export default Footer
