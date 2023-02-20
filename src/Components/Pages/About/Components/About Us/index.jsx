39ff8ab18c1b1fd388173cbaf7c94b480179a4e7
JoeFikis <aidanmg608@gmail.com>
2023-02-19 22:32:06 -0600

    work

---
+++ src/Components/Pages/About/Components/About Us/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+        <div className="about_us-container">
+            <div className="about_us-wrapper">
+                <div className="about_us-header">
+                    <h1>About Us</h1>
+                </div>
+                <div className="about_us-description">
+                    <h2>
+                        About us. We are a server host offering many services. Such as minecraft servers, factorio, and other services.
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default index
